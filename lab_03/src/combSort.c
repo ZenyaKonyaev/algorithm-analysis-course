5a1d5b06602c14fb0647e9d350987ec396f6a137
ZenyaKonyaev <aa068555960@gmail.com>
2022-09-29 00:08:00 +0300

    done estimate and bucket shema

--- lab_03/src/combSort.c
+++ lab_03/src/combSort.c
@@ -5,22 +5,27 @@
 #include "combSort.h"
 #include "time.h"
 
+
+//4 2 9 1 6
 int combSort(sortElem *arr, size_t n)
 {
     clock_t timeStart = clock();
 
     float factor = 1.247f; // оптимальное число для вычисления шага сравнения
     size_t gap = (size_t)(n / factor); //шаг между элементами
-    while (gap >= 1) {
+    int swapped = 1;
+    while (gap != 1 || swapped) {
+        swapped = 0;
         for(size_t i = 0, j = gap; j < n; ++i, ++j)
         {
             if (comparatorIncrease(arr + i, arr + j)) {
                 sortElem tmp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = tmp;
+                swapped = 1;
             }
         }
-        gap /= factor;
+        gap = gap / factor < 1 ? 1 : gap / factor;
     }
 
     return clock() - timeStart;
