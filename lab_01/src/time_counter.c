d35ab3852aa23cc5a2ec942e25b5a4f7cd423cdb
ZenyaKonyaev <aa068555960@gmail.com>
2022-10-22 16:22:14 +0300

    done first lab code and report

--- lab_01/src/time_counter.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by zenya2602 on 21.10.22.
-//
-
-#include "time_counter.h"
--- lab_01/src/time_counter.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by zenya2602 on 21.10.22.
-//
-
-#ifndef SRC_TIME_COUNTER_H
-#define SRC_TIME_COUNTER_H
-
-#endif //SRC_TIME_COUNTER_H
